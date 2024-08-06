3992d680bbe8f3914e1fa2cb2fd6af4e3bbd51b1
sarkis <sarkis.gmail.com>
2024-08-06 13:43:18 +0300

    c6

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 function getStudents(){
-    fetch('https://session7api.onrender.com')
+    fetch('https://session7api.onrender.com/students')
       .then(response => response.json())
       .then(data => {
-var select_users=document.getElementById('select_users')
-data.array.forEach(st => {
+var select_users=document.getElementById('select_users');
+data.forEach(st => {
   var opt =document.createElement('option')  ;
   opt.innerHTML= st.name;
   select_users.appendChild(opt);
---
+++ test.js
@@ -0,0 +1,5 @@
+ /*setTimeout(() => {
+    console.log('A')
+}, 2000);// in milliesecondes
+console.log('B')
+console.log('C')// after 2s bytb3 A;*/
